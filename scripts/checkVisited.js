34da86c3d9997f404bcf1f0e77769170dd53768e
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-06-19 23:36:01 -0600

    Fixing drop down menu glitches

--- scripts/checkVisited.js
+++ scripts/checkVisited.js
@@ -1,21 +1,13 @@
 const checkVisited = () => {
     const animationBox = document.getElementById('anim');
-    const nav = document.getElementsByClassName('welcomeNav');
-    const main = document.getElementsByClassName('welcomeMain');
-    const greet = document.getElementsByClassName('welcomeGreet');
-    const delayed = document.getElementsByClassName('delayedGreet');
-    const bio = document.getElementsByClassName('welcomeBio');
-    const description = document.getElementsByClassName('welcomeDescription');
+    const nav = document.getElementsByClassName('welcomeNav')[0];
+    const main = document.getElementsByClassName('welcomeMain')[0];
 
     if (sessionStorage.getItem('visited') === 'true') {
         animationBox.removeAttribute('id');
         animationBox.setAttribute('id', 'noAnim');
         nav.style.removeProperty('animation');
         main.style.removeProperty('animation');
-        greet.style.removeProperty('animation');
-        delayed.style.removeProperty('animation');
-        bio.style.removeProperty('animation');
-        description.style.removeProerty('animation');
     } 
 
     return 'ready';
